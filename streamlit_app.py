43229e4fad680da305c4a7e47186cd7770d5f72c
shirasagiK <85680054+shirasagiK@users.noreply.github.com>
2023-10-12 15:19:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 uploaded_file = st.file_uploader("アクセスログをアップロードしてください")
 
 import pandas as pd
-if uploaded_file is not Noe:
+if uploaded_file is not None:
   df = pd.read_csv(
       uploaded_file,
       sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
